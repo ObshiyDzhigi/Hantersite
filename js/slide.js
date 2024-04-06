d9b1f9c72ac9c7bd82a7517fdd55994da779d70d
zhigazh2017@outlook.com <zhigazh2017@outlook.com>
2024-04-06 18:41:38 +0500

    first commit

---
+++ js/slide.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nextButton = document.getElementById('nextButton');
+    const slider = document.getElementById('slider');
+    const cards = document.querySelectorAll('.reviews__column');
+    const cardWidth = cards[0].offsetWidth;
+    const firstl = document.querySelector('.reviews_firstl');
+
+    nextButton.addEventListener('click', function() {
+        const scrollLeft = slider.scrollLeft;
+        const visibleWidth = slider.offsetWidth;
+        const contentWidth = slider.scrollWidth;
+        const cardsPerRow = Math.floor(visibleWidth / cardWidth);
+        const nextScroll = scrollLeft + visibleWidth;
+
+        // Проверяем, сколько карточек осталось скрытыми вправо
+        const remainingCards = cards.length - Math.ceil(nextScroll / cardWidth);
+
+        // Если осталось меньше трех карточек, возвращаемся к началу
+        if (remainingCards <= 0) {
+            slider.scrollTo({
+                left: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            // Иначе прокручиваем к следующим трем карточкам
+            slider.scrollTo({
+                left: nextScroll,
+                behavior: 'smooth'
+            });
+        }
+
+        // Добавляем класс, который инициирует анимацию
+        firstl.classList.add('half-fill');
+
+        // После завершения анимации, убираем класс и возвращаем фон в исходное состояние
+        setTimeout(() => {
+            firstl.classList.remove('half-fill');
+        }, 500); // Время анимации, в данном случае 0.5 секунды (500 миллисекунд)
+    });
+});
